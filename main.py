e3a2ea06d59ce9709f2fa2802a3957245ccb3c54
A-Brambilla <103998377+A-Brambilla@users.noreply.github.com>
2023-07-26 05:59:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Welcome to my Band name generator")
+
+print("Which city did you grow up in?")
+city = input()
+
+print("What is the name of a pet?")
+pet = input()
+
+bandName = city + " " + pet
+print("You should name your band:", bandName)
